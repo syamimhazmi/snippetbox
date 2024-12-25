9a837fda5016f2a40ebbd1414455bce921ae7448
syamimhazmi <syamimhazmi@gmail.com>
2024-12-25 15:01:14 +0700

    view snippets data for home page

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/syamimhazmi/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
